362d83cca705c91c8f9984868a2a679212bdbde1
Chun-an Chan <chunanchan@gmail.com>
2018-08-19 11:59:13 +0100

    Add docstring for Solver.

--- logic_solver.py
+++ logic_solver.py
@@ -48,8 +48,46 @@ def PossiblyEq(xs):
 
 
 class Solver(object):
+  """Finds variable values that satisfies all predicates.
+  
+  Given a set a variables X = (X1, X2, ..., Xn), each of which \in 
+  {1, 2, ..., K} and a list of predicates P = (P1, P2, ..., Pm} where
+  each Pi evaluates the truth value given X. Find all possible X's that
+  satisfy P. Note that we assume n == m (num_predicates) and K = num_options.
+  
+  Predicates are implemented as function that takes a getter, which returns
+  the value given the index of X. E.g.:
+  
+  def predicate1(getter):
+    # Returns True if X1 == X2.
+    return getter(0) == getter(1)  # Note that getter are 0-based.
+    
+  self.dependent is a map from predicate to indices in X indicating that
+  the predicate only dependents on these indices in X. The dependency is
+  evaluated by running the predicate, so the predicate function should
+  have all its dependencies called before any condition clause. E.g.
+  
+  def predicate2(getter):
+    # Returns True if X1 == X2 == X3.
+    x1, x2, x3 = [getter(i) for i in (0, 1, 2)]
+    return x1 == x2 and x2 == x3
+  
+  But not as follows:
+  
+  def bad_predicate2(getter):
+    if getter(0) != getter(1) or getter(1) != getter(2):
+      return False
+    return True
+  """
 
   def __init__(self, predicates, num_options):
+    """Constructor.
+    
+    Args:
+      predicates: (list of func(getter)->bool), each of which returns False when 
+        the partial set of values (obatined via getter) violates the predicate,
+        or return True otherwise.
+      num_options: (int) number of possible values for each variable.
     self.num_predicates = len(predicates)
     self.predicates = predicates[:]
     self.answers = [0] * self.num_predicates
